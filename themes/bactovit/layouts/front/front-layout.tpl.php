3cca902fda905a156f78e5aab1f8bdecab90db8e
web-padawan <skamandr@yandex.ru>
2016-03-20 17:51:34 +0200

    [new] about block, modal popups

--- themes/bactovit/layouts/front/front-layout.tpl.php
+++ themes/bactovit/layouts/front/front-layout.tpl.php
@@ -39,6 +39,190 @@
     </div>
   </header>
 
+  <div class="about">
+    <div class="about__inner">
+
+      <div class="about__flag flag">&nbsp;</div>
+
+      <div class="about__title"><span class="about__brand">БактоВіт</span> - пробіотик з повним комплексом вітамінів группи В</div>
+
+      <ul class="about__goals">
+        <li class="about__goal about__goal--1">
+          <div class="about__picture">
+            <div class="about__circle about__circle--gold">&nbsp;</div>
+          </div>
+          <div class="about__text">для нормалізації мікрофлори кишківника і процесів травлення під час або після прийому антибіотиків</div>
+        </li>
+        <li class="about__goal about__goal--2">
+          <div class="about__picture">
+            <div class="about__circle about__circle--blue">&nbsp;</div>
+          </div>
+          <div class="about__text">при зміні раціону харчування, в тому числі під час подорожей</div>
+        </li>
+        <li class="about__goal about__goal--3">
+          <div class="about__picture">
+            <div class="about__circle about__circle--gold">&nbsp;</div>
+          </div>
+          <div class="about__text">для попередження розвитку дисбактеріозу</div>
+        </li>
+        <li class="about__goal about__goal--4">
+          <div class="about__picture">
+            <div class="about__circle about__circle--blue">&nbsp;</div>
+          </div>
+          <div class="about__text">при шлунково - кишкових розладах (діареї , запору)</div>
+        </li>
+        <li class="about__goal about__goal--5">
+          <div class="about__picture">
+            <div class="about__circle about__circle--gold">&nbsp;</div>
+          </div>
+          <div class="about__text">для відновлення імунітету</div>
+        </li>
+      </ul>
+
+      <ul class="about__list">
+        <li class="about__item about__item--1">
+          <div class="about__vitamin">Вітамін В1</div>
+          <div class="about__text">Регулює обмін вуглеводів і жирів (ліпідів) у всіх органах і тканинах організму людини</div>
+          <a class="about__link" data-target="B1" href="#">Детальніше →</a>
+        </li>
+        <li class="about__item about__item--2">
+          <div class="about__vitamin">Вітамін В2</div>
+          <div class="about__text">«Шкірний» вітамін. Відомо, що здоров'я і загальний стан організму людини залежать від його шкіри</div>
+          <a class="about__link" data-target="B2" href="#">Детальніше →</a>
+        </li>
+        <li class="about__item about__item--3">
+          <div class="about__vitamin">Вітамін В5</div>
+          <div class="about__text">є структурним компонентом одного з ключових речовин метаболізму&nbsp;-&nbsp;коферменту&nbsp;А</div>
+          <a class="about__link" data-target="B5" href="#">Детальніше →</a>
+        </li>
+        <li class="about__item about__item--4">
+          <div class="about__vitamin">Вітамін В6</div>
+          <div class="about__text">Надає наступні фізіологічні ефекти в організмі людини: регуляція білкового обміну</div>
+          <a class="about__link" data-target="B6" href="#">Детальніше →</a>
+        </li>
+        <li class="about__item about__item--5">
+          <div class="about__vitamin">Вітамін В12</div>
+          <div class="about__text">В організмі людини надає наступні фізіологічні ефекти: забезпечує утворення еритроцитів</div>
+          <a class="about__link" data-target="B12" href="#">Детальніше →</a>
+        </li>
+      </ul>
+
+      <div class="jqmWindow vitpopup" id="popup-vitamin-B1">
+        <div class="jqmContentWrapper">
+          <div class="jqmContent clearfix modal">
+            <section class="modal">
+              <h3 class="modal__title">Вітамін B1</h3>
+              <p class="modal__text"><b>Джерело:</b></p>
+              <p class="modal__text">
+                <span>Cвинина,печінка, нирки, картопля, крупи (вівсяна, пшенична, гречана),</span>
+                <span>хліб (ржаний та цільнозерновий), зелений горошок.</span>
+              </p>
+              <p class="modal__text modal__text--func"><b>Основні функції:</b></p>
+              <p class="modal__text">
+                <span>Необхідна складова безлічі ферментів, які в основному використовуються</span>
+                <span>для отримання енергії із їжі. Потрібен для синтезу білків та жирів.</span>
+                <span>Впливає на обмін речовин, що беруть участь у передачі нервового</span>
+                <span>імпульсу, необхідний для процесів скорочення м'язів. Нормалізує</span>
+                <span>мозковий кровообіг, підвищує здатність людини до навчання,</span>
+                <span>а крім того, бере участь у процесах кровотворення.</span>
+              </p>
+            </section>
+          </div>
+          <a href="javascript:void(0);" class="jqmClose" title="Закрыть окно"></a>
+        </div>
+      </div>
+
+      <div class="jqmWindow vitpopup" id="popup-vitamin-B2">
+        <div class="jqmContentWrapper">
+          <div class="jqmContent clearfix modal">
+            <section class="modal">
+              <h3 class="modal__title">Вітамін B2</h3>
+              <p class="modal__text"><b>Джерело:</b></p>
+              <p class="modal__text">
+                <span>Печінка, нирки, сир, шипшина, незбиране молоко, бобові, зелений горошок,</span>
+                <span>м'ясо, крупи (гречана, вівсяна), хліб з борошна грубого помелу, шпинат.</span>
+              </p>
+              <p class="modal__text modal__text--func"><b>Основні функції:</b></p>
+              <p class="modal__text">
+                <span>Забезпечує здоров’я та гарний стан шкіри. Допомагає імунній</span>
+                <span>системі, зберігає в хорошій формі слизові оболонки, що</span>
+                <span>покривають дихальну і травну системи. Допомагає виробляти </span>
+                <span>антитіла для боротьби зі шкідливими мікроорганізмами.</span>
+              </p>
+            </section>
+          </div>
+          <a href="javascript:void(0);" class="jqmClose" title="Закрыть окно"></a>
+        </div>
+      </div>
+
+      <div class="jqmWindow vitpopup" id="popup-vitamin-B5">
+        <div class="jqmContentWrapper">
+          <div class="jqmContent clearfix modal">
+            <section class="modal">
+              <h3 class="modal__title">Вітамін В5</h3>
+              <p class="modal__text"><b>Вітамін В5 (Пантотенова кислота)</b></p>
+              <p class="modal__text">
+                <span>є структурним компонентом одного з ключових речовин метаболізму - коферменту А,</span>
+                <span>який бере участь у всіх основних видах обміну - білковому, ліпідному і вуглеводному,</span>
+                <span>процесах детоксикації, активуючи і переносячи ацильні (в тому числі ацетильні) залишки.</span>
+                <span>Ацетил кофермент бере участь в синтезі нейромедіатора ацетилхоліну, кортикостероїдів,</span>
+                <span>гемоглобіну, холіну та інших з'єднань.</span>
+              </p>
+            </section>
+          </div>
+          <a href="javascript:void(0);" class="jqmClose" title="Закрыть окно"></a>
+        </div>
+      </div>
+
+      <div class="jqmWindow vitpopup" id="popup-vitamin-B6">
+        <div class="jqmContentWrapper">
+          <div class="jqmContent clearfix modal">
+            <section class="modal">
+              <h3 class="modal__title">Вітамін B6</h3>
+              <p class="modal__text"><b>Джерело:</b></p>
+              <p class="modal__text">
+                <span>Печінка, нирки, птиця, м'ясо, риба, бобові, диня, крупи (гречана, пшоняна,</span>
+                <span>ячна), перець, картопля, хліб (з борошна грубого помелу), гранат.</span>
+              </p>
+              <p class="modal__text modal__text--func"><b>Основні функції:</b></p>
+              <p class="modal__text">
+                <span>Необхідний для функціонування близько ста життєво важливих ферментів, які є</span>
+                <span>каталізаторами важливих хімічних реакцій людського організму. Бере участь</span>
+                <span>у процесах синтезу серотоніну&nbsp;— речовини, яку називають «гормоном щастя».</span>
+                <span>Серотонін контролює емоції, апетит і сон людини. Необхідний для синтезу </span>
+                <span>нуклеїнових кислот, а також для синтезу гема, компонента гемоглобіну.</span>
+              </p>
+            </section>
+          </div>
+          <a href="javascript:void(0);" class="jqmClose" title="Закрыть окно"></a>
+        </div>
+      </div>
+
+      <div class="jqmWindow vitpopup" id="popup-vitamin-B12">
+        <div class="jqmContentWrapper">
+          <div class="jqmContent clearfix modal">
+            <section class="modal">
+              <h3 class="modal__title">Вітамін B12</h3>
+              <p class="modal__text"><b>Джерело:</b></p>
+              <p class="modal__text">
+                <span>Печінка, нирки, м’ясо, риба, сир.</span>
+              </p>
+              <p class="modal__text modal__text--func"><b>Основні функції:</b></p>
+              <p class="modal__text">
+                <span>Бере участь у регуляції процесу обміну білків і нуклеїнових кислот.</span>
+                <span>Сприяє зростанню і відновленню тканин. Необхідний для нормального</span>
+                <span>функціонування нервової системи. Необхідний для синтезу гемоглобіну&nbsp;—</span>
+                <span>переносника кисню від легенів до інших тканин організму.</span>
+              </p>
+            </section>
+          </div>
+          <a href="javascript:void(0);" class="jqmClose" title="Закрыть окно"></a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
   <div class="contact">
     <div class="contact__inner">
 
